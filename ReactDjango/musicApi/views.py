9fb12df50203dbab3baf89a1bd41b4f1af8cd793
Michael <michael27mic@gmail.com>
2024-06-06 11:08:19 +0100

    setup for room api

--- ReactDjango/musicApi/views.py
+++ ReactDjango/musicApi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
+def main(request):
+    return HttpResponse("Hello, Devs!")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
