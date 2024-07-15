c5b29a84d90f0fb2994dd0c82c87f95b308d30df
Michael <michael27mic@gmail.com>
2024-07-15 19:33:07 +0100

    calling API from React

--- ReactDjango/musicApi/urls.py
+++ ReactDjango/musicApi/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RoomView.as_view()),
+    path('room', views.RoomView.as_view()),
+    path('create-room', views.CreateRoomView.as_view()),
+    path('get-room', views.GetRoom.as_view()),
 ]
